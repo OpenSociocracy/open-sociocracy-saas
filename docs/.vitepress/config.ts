49b7804059add09ca7a3f283db154c10334c8ac2
Brian Winkers <bwinkers@gmail.com>
2023-05-05 00:11:02 -0700

    Remove broken Discord link
    Only until we can setup the welcome channel and all that.

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
 
     socialLinks: [
       { icon: 'github', link: 'https://github.com/orgs/OpenSociocracy' },
-      { icon: 'discord', link: 'https://discord.gg/seExDy6M'},
+      // { icon: 'discord', link: 'https://discord.gg/seExDy6M'},
       { icon: 'twitter', link: 'https://twitter.com/OpenSociocracy'},
       { icon: 'facebook', link: 'https://www.facebook.com/OpenSociocracy/'},
       { icon: 'mastodon', link: 'https://fosstodon.org/@opensociocracy'}
