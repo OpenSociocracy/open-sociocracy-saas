c62b1ebe94a1eb301b6ae50a8c966f96ae66c861
Brian Winkers <bwinkers@gmail.com>
2023-05-06 23:32:56 -0700

    Add footer

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -17,6 +17,11 @@ export default defineConfig({
       { icon: 'twitter', link: 'https://twitter.com/OpenSociocracy'},
       { icon: 'facebook', link: 'https://www.facebook.com/OpenSociocracy/'},
       { icon: 'mastodon', link: 'https://fosstodon.org/@opensociocracy'}
-    ]
+    ],
+
+    footer: {
+      message: 'Sponsored by "S3 org name" and "funding org name"',
+      copyright: 'Released under the <a href="https://github.com/OpenSociocracy/documentation-website/blob/main/LICENSE">MIT</a> and <a href="https://github.com/OpenSociocracy/open-sociocracy-saas/blob/main/LICENSE">AGPL</a> License</a>.<br />Copyright <a href="https://creativecommons.org/licenses/by-sa/4.0/">CC BY-SA</a>'
+    }
   }
 })
